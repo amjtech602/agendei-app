6426993c2b80f30ede868345b5594acaf232419d
Antonio Filho <atnfilho@yahoo.com.br>
2025-08-20 19:58:01 -0300

    Teste de push

--- src/app/painel/page.tsx
+++ src/app/painel/page.tsx
@@ -20,6 +20,7 @@ export default function PainelProfissional() {
             a.servico.toLowerCase().includes(search.toLowerCase());
         const matchesStatus = statusFilter === "Todos" || a.status === statusFilter;
         return matchesSearch && matchesStatus;
+        
     });
 
     return (
